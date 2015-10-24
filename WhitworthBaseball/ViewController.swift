082747d53fd45491884479a22061b1a87d0acc0c
joronia <zacharypgoold@Zachs-MacBook-Pro.local>
2015-10-23 21:36:53 -0700

    Created GUI Objects
    Added functions for ListView but not working

--- WhitworthBaseball/ViewController.swift
+++ WhitworthBaseball/ViewController.swift
@@ -8,17 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+ //Properties
+    @IBOutlet weak var QueryChoices: UIPickerView!
+    @IBOutlet weak var DisplayQuery: UITableView!
+    @IBOutlet weak var SubmitQuery: UIButton!
+    
+    // Declaring a new array to hold choice and allow access to
+    //outside functions
+    var PickerData:[String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // connect data
+        self.QueryChoices.delegate = self
+        self.QueryChoices.dataSource = self
+        
+        
+        // Input data into the Array:
+        
+        PickerData = ["Query 1", "Query 2", "Query 3", "Query 4", "Query 5"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+       return 1
+    }
+    
+    //The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return PickerData.count
+    }
+    
+    //The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+       return PickerData[row]
+    }
+    
+    // Capture the picker view selection
+    //func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+      
+    //}
+    
+    
+    
+    
 
 
 }
