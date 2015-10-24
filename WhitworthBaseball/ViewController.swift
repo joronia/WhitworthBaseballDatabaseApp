6effd25a4c5bda133d2491b0bd325d61c0eaeaf7
Zachary P Goold <zacharypgoold@zachs-macbook-pro.local>
2015-10-23 18:18:00 -0700

    Initial Commit

---
+++ WhitworthBaseball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhitworthBaseball
+//
+//  Created by Zachary P Goold on 10/23/15.
+//  Copyright © 2015 Zachary P Goold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
